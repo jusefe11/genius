526216c0c01b6a8e8d2de1c59171f81a38d51e5f
Juan Ferrer <jusef11@gmail.com>
2026-01-10 12:58:31 -0500

    feat: actualizar infraestructura Terraform

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -112,7 +112,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     # Usa el NAT Gateway correspondiente a la misma AZ (mismo índice)
     nat_gateway_id = aws_nat_gateway.main[count.index % length(aws_nat_gateway.main)].id
   }
